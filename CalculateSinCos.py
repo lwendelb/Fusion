1266d1b63f838c2d3d4239d49a35768a60d96a4e
lwendelb <34950982+lwendelb@users.noreply.github.com>
2018-05-24 11:44:29 -0400

    added adaptive step size during burn-in

--- CalculateSinCos.py
+++ CalculateSinCos.py
@@ -2,27 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def CalcSin(x, params, share):
-    [A,phi] = params
-    lam = share
+    [lam,phi] = share
+    A = params
     y = A*np.sin(lam*x+phi)
     return y
 def CalcCos(x, params, share):
-    [A,phi] = params
-    lam = share
+    [lam,phi] = share
+    A = params
     y = A*np.cos(lam*x+phi)
     return y
 
 lam = np.pi
 A_sin = 8.
 A_cos = 4.7
-phi_sin = np.pi/3
-phi_cos = np.pi/2
+phi = np.pi/4
 
 x = np.arange(0.,2*np.pi,0.01)
 
-truth_sin = np.array([A_sin,phi_sin])
-truth_cos = np.array([A_cos,phi_cos])
-share = np.array([lam])
+truth_sin = np.array([A_sin])
+truth_cos = np.array([A_cos])
+share = np.array([lam,phi])
 
 ysin_mean = CalcSin(x,truth_sin,share)
 ycos_mean = CalcCos(x,truth_cos,share)
--- mainSC.py
+++ mainSC.py
@@ -20,6 +20,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import CalculateSinCos as Calc
 import FmcmcSC as MCMC
+import csv
 
 def par2z(par,lower,upper):
     z = norm.ppf((par-lower)/(upper-lower))
@@ -35,14 +36,20 @@ def z2par(z, lower, upper, grad=False):
         par[np.array([par[j]==lower[j] for j in range(len(par))])] += 1e-10
         return par
 
-paramList = ["Lam","Asin","phisin","Acos","phicos"]
+#paramList = ["Lam","Asin","phisin","Acos","phicos"]
+paramList = ["Lam","Phi","Asin","Acos"]
+#paramList = ["Lamsin","Asin","Phisin","Lamcos","Acos","Phicos"]
 
 truth = np.concatenate((Calc.share,Calc.truth_sin,Calc.truth_cos))
 print truth
-lower = np.array([0,0,0,0,0])
-upper = np.array([2*np.pi,20,np.pi,10,np.pi])
-params = np.array([np.pi/2,4,1,6,3])
-init_z = par2z(params,lower,upper)
+lower = np.array([0,0,0,0])
+upper = np.array([2*np.pi,np.pi,20,10])
+#lower = np.array([0,0,0])
+#upper = np.array([2*np.pi,20,np.pi])
+#lower = np.array([0,0,0,0,0,0])
+#upper = np.array([2*np.pi,20,np.pi,2*np.pi,10,np.pi])
+#params = np.array([np.pi/4,np.pi,1,3])
+#init_z = par2z(params,lower,upper)
 init_z = par2z(truth*0.9,lower,upper)
 
 print init_z
@@ -50,40 +57,52 @@ print z2par(init_z,lower,upper)
 
 bx = 1
 bn = 1
-taux = 10
-taun = 10
-yx = np.random.normal(Calc.ysin_mean,1/np.sqrt(taux))
-yn = np.random.normal(Calc.ycos_mean,1/np.sqrt(taun))
+taux = 4.
+taun = 2.
+#taux = 4.
+#taun=2.
+#yx = np.random.normal(Calc.ysin_mean,1/np.sqrt(taux))
+#yn = np.random.normal(Calc.ycos_mean,1/np.sqrt(taun))
+yx = np.random.normal(Calc.ysin_mean,np.sqrt(np.abs(Calc.ysin_mean/8.))/np.sqrt(taux))
+yn = np.random.normal(Calc.ycos_mean,np.sqrt(np.abs(Calc.ycos_mean/4.7))/np.sqrt(taun))
+
 
 '''
 plt.figure(1)
 plt.plot(Calc.x,Calc.ysin_mean)
 plt.plot(Calc.x,yx,'r+')
-plt.ylabel("Intensity")
-plt.xlabel("Scattering Angle")
+plt.ylabel("Amplitude")
+plt.xlabel("x")
 plt.figure(2)
 plt.plot(Calc.x,Calc.ycos_mean)
 plt.plot(Calc.x,yn,'r+')
-plt.ylabel("Intensity")
-plt.xlabel("Scattering Angle")
+plt.ylabel("Amplitude")
+plt.xlabel("x")
 plt.show()
 '''
 
-curr=10000
-burn = 0
+curr= 40000
+burn = 10000
 results = MCMC.nlDRAM(paramList=paramList, init_z=init_z, lower=lower,
-upper=upper, y_x=yx, y_n=yn, x=Calc.x, L=20, shrinkage=0.01, s_p=(2.4**2),
-epsilon=1e-10, m0=0, sd0=1, c_y=0.1, d_y=0.1, c_g=0.1, d_g=0.1, c_b=0.1,
+upper=upper, y_x=yx, y_n=yn, x=Calc.x, L=20, shrinkage=0.007, s_p=(2.4**2),
+epsilon=1e-10, m0=0, sd0=1, c_y=0.1, d_y=0.1, c_g=1, d_g=1, c_b=0.1,
 d_b=0.1, adapt=200, thin=1, iters=curr, burn=burn, update=1000, plot=False, fix=False)
-import csv
-with open('C:\Users\Laura\Documents\R\Research\sc.csv', 'wb') as f:
+
+with open('C:\Users\Laura\Documents\Fusion-master\/temp\/results.csv', 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(results[0])
+
+with open('C:\Users\Laura\Documents\Fusion-master\/temp\/taux.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerows(np.array([results[2]]))
+with open('C:\Users\Laura\Documents\Fusion-master\/temp\/taun.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerows(np.array([results[3]]))
 
 '''
 results = MCMC.nlDRAM(paramList, init_z, lower, upper, y_x=yx, y_n=yn, x=Calc.x, L=20, shrinkage=0.2, s_p=(2.4**2), epsilon=1e-4, m0=0, sd0=1, c_y=0.1, d_y=0.1, c_g=0.1, d_g=0.1, c_b=0.1, d_b=0.1, adapt=20, thin=1, iters=100, burn=0, update=10, plot=False, fix=False)
 import csv
-with open('C:\Users\Laura\Documents\R\Research\pars2.csv', 'wb') as f:
+with open('C:\Users\Laura\Documents\R\Research\sincosresults_5_10_2018_9_52.csv', 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(results[0])
 with open('C:\Users\Laura\Documents\R\Research\VarS12.csv', 'wb') as f:
@@ -114,11 +133,29 @@ plt.plot(keep_params[range(curr_keep), 3], 'k')
 plt.plot(np.repeat(truth[3],curr_keep),'r')
 plt.xlabel("Iteration")
 plt.ylabel(paramList[3])
+'''
 plt.subplot(235)
 plt.plot(keep_params[range(curr_keep), 4], 'k')
 plt.plot(np.repeat(truth[4],curr_keep),'r')
 plt.xlabel("Iteration")
 plt.ylabel(paramList[4])
+plt.subplot(236)
+plt.plot(keep_params[range(curr_keep), 5], 'k')
+plt.plot(np.repeat(truth[5],curr_keep),'r')
+plt.xlabel("Iteration")
+plt.ylabel(paramList[5])
+'''
+
+plt.figure(4, figsize=(20,10))
+plt.subplot(121)
+plt.plot(results[2], 'k')
+plt.xlabel("Iteration")
+plt.ylabel("taux")
+plt.subplot(122)
+plt.plot(results[3], 'k')
+plt.xlabel("Iteration")
+plt.ylabel("taun")
+
 '''
 plt.subplot(236)
 plt.plot(keep_params[range(curr_keep), 5], 'k')
